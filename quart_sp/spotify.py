a55a0581c48a30ba03327c11ff8b9a77d0ca7c63
kawadeomkar <kawade.omkar21@gmail.com>
2020-12-21 21:37:05 -0800

    purge spotipy -- asyncio aiohttp spotify class

--- quart_sp/spotify.py
+++ quart_sp/spotify.py
@@ -1,90 +1,149 @@
-from typing import List, Union
+from typing import Any, Dict, List, Union
+
 import aiohttp
+import asyncio
+import util
+
+log = util.setLogger(__name__)
+
 
 class Spotify:
+    endpoint_prefix = "https://api.spotify.com/"
 
-    endpoint = "https://api.spotify.com/"
-    
     def __init__(self, auth_token: str):
         self.auth_token = auth_token
+        self.user_id = None
 
+    async def http_call(self,
+                        endpoint_route: str,
+                        session: aiohttp.ClientSession,
+                        params: Dict[str, Union[str, int]] = None,
+                        http_method='GET'):
+        async with session.request(http_method,
+                                   self.endpoint_prefix + endpoint_route,
+                                   headers={'Authorization': f"Bearer {self.auth_token}",
+                                            'Content-Type': 'application/json'},
+                                   params=params) as resp:
+            if resp.status == 200 or resp.status == 201:
+                data = await resp.json()
+                return data
 
-    def current_user_saved_tracks(self, limit: int, offset: int):
+    async def get_user_id(self, session: aiohttp.ClientSession) -> str:
         """
-        docstring
+        Returns user's spotify ID
         """
-        endp = "v1/me/tracks"
-        pass
+        if self.user_id is not None:
+            return self.user_id
 
-    
-    def create_playlist(self, user_id: str, 
-                              name: str, 
-                              public: bool = True, 
-                              description: str = 'This playlist was curated by SpotifyAnalytics https://github.com/kawadeomkar/spotify-analytics'):
-        """
-        docstring
-        """
-        endp = f"v1/users/{user_id}/playlists"
-        pass
+        endpoint_route = "v1/me"
+        resp = await self.http_call(endpoint_route, session)
+        self.user_id = resp['id']
+        return self.user_id
 
-    def add_items_to_playlist(self, playlist_id: str, tracks: List[str],  position: int = None):
+    async def current_user_saved_tracks(self, limit: int, offset: int,
+                                        session: aiohttp.ClientSession) -> List[Dict[str, Any]]:
         """
-        docstring
-        """
-        endp = f"v1/playlists/{playlist_id}/tracks"
-        pass
+        Returns saved tracks from the user's library
 
-
-    def artists(self, ids: Union[List, str]):
+        limit: number of desired tracks (max=50)
+        offset: offset of saved library to start from
         """
-        docstring
-        """
-        endp = "v1/artists"
-        if isinstance(ids, list):
-            pass
-        elif isinstance(ids, str):
-            pass
+        endpoint_route = "v1/me/tracks"
+        resp = await self.http_call(endpoint_route, session,
+                                    params={'limit': limit, 'offset': offset})
+        return resp['items']
 
-
-    def albums(self, ids: Union[List, str]):
+    async def create_playlist(self, user_id: str,
+                              name: str,
+                              session: aiohttp.ClientSession,
+                              public: bool = True,
+                              description: str = 'This playlist was curated by SpotifyAnalytics '
+                                                 'https://github.com/kawadeomkar/spotify-analytics') -> str:
         """
-        docstring
+        Creates a playlist, returns id of playlist upon creation
         """
-        endp = "v1/albums"
-        if isinstance(ids, list):
-            pass
-        elif isinstance(ids, str):
-            pass
-    
+        endpoint_route = f"v1/users/{user_id}/playlists"
+        resp = await self.http_call(endpoint_route,
+                                    session,
+                                    params={'name': name, 'public': public,
+                                            'description': description},
+                                    http_method='POST')
+        return resp['id']
 
-    def get_devices(self):
+    async def add_items_to_playlist(self, playlist_id: str,
+                                    tracks: List[str],
+                                    session: aiohttp.ClientSession):
         """
-        docstring
+        Add items to a playlist, returns snapshot id (used to identify playlist version)
         """
-        endp = "v1/me/player/devices"
-        pass
+        endpoint_route = f"v1/playlists/{playlist_id}/tracks"
+        resp = await self.http_call(endpoint_route, session, params={'uris': tracks},
+                                    http_method='POST')
+        return resp['snapshot_id']
 
+    async def artists(self, ids: Union[List, str], session: aiohttp.ClientSession):
+        """
+        Returns artist(s) information
 
-    def play_song(self, context_uri: str,
-                        uris: List[str],
+        ids: List of artist IDs or single artist ID
+        """
+        endpoint_route = "v1/artists"
+        if isinstance(ids, str):
+            ids = [ids]
+        resp = await self.http_call(endpoint_route, session, params={'ids': ids})
+        return resp['artists']
+
+    async def albums(self, ids: Union[List, str], session: aiohttp.ClientSession):
+        """
+        Returns album(s) information
+
+        ids: List of album IDs or single album ID
+        """
+        endpoint_route = "v1/albums"
+        if isinstance(ids, str):
+            ids = [ids]
+        resp = await self.http_call(endpoint_route, session, params={'ids': ids})
+        return resp['albums']
+
+    async def get_devices(self, session: aiohttp.ClientSession):
+        """
+        Returns active devices
+        """
+        endpoint_route = "v1/me/player/devices"
+        resp = await self.http_call(endpoint_route, session)
+        return resp['devices']
+
+    async def play_song(self, session: aiohttp.ClientSession,
+                        context_uri: str = None,
+                        uris: List[str] = None,
                         offset: Dict[str, Union[str, int]] = None,
                         position_ms=None):
-    """
-    Plays a song if uris is provided, otherwise plays context_uri if provided and then returns
-    true. Otherwise returns false
-    """
-    if uris is None and context_uri is None:
-        return False
 
-    endp = "v1/me/player/play"
-    try:
-        # uris take priority
+        """
+        Plays a song if uris is provided, otherwise plays context_uri if provided and then returns
+        true. Otherwise returns false
+
+        offset: plays uri with offset - applies only to albums and playlist
+        """
+        if uris is None and context_uri is None:
+            return False
+
+        endpoint_route = "v1/me/player/play"
+        params = {}
+
+        if offset:
+            params['offset'] = offset
+        if position_ms:
+            params['position_ms'] = position_ms
         if uris:
-            pass
+            params['uris'] = uris
         elif context_uri:
-            pass
-        return True
-    # TODO: aiohttp err fix
-    except HTTPError as e:
-        log.error(e)
-        return False
+            params['context_uri'] = context_uri
+
+        resp = await self.http_call(endpoint_route, session, params=params, http_method='PUT')
+
+        if resp is None:
+            return True
+        else:
+            log.error(resp)
+            return False
